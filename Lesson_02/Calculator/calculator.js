cf5aed92574b02d0082e763a6328918c6af363a7
Amanda <blayla89@gmail.com>
2024-05-02 14:22:32 -0500

    Extracts valid operations and sets as constant in top-level of program

--- Lesson_02/Calculator/calculator.js
+++ Lesson_02/Calculator/calculator.js
@@ -2,6 +2,7 @@
 
 const READLINE = require('readline-sync');
 const MESSAGES = require('./calculator_messages.json');
+const VALID_OPERATIONS = ['1', '2', '3', '4']; 
 let defaultLanguage = 'en';
 const LANGUAGES = ['en', 'fr'];
 let play = true;
@@ -9,22 +10,27 @@ let play = true;
 // FXNS _______________________________________________
 
 function invalidNumber(number) {
+  // checks if number IS an empty or whitespace string OR IS  NaN => returns boolean (true if number IS invalid)
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
 function invalidOperation(operation) {
-  return !['1', '2', '3', '4'].includes(operation);
+  // checks operation input -> returns true if it is NOT included in the string
+  return !VALID_OPERATIONS.includes(operation);
 }
 
 function prompt(message, variable) {
+  // checks if the variable is undefined, and if yes it outputs message as a string
   if (variable === undefined) {
     console.log(`=> ${message}`);
+  // if the variable is not empty, then it outputs the a more complex message AND the variable 
   } else {
     console.log(`=> ${message} ${variable}`);
   }
 }
 
 function performOperation(number1, number2, operation) {
+  // takes 3 inputs to perform operation and dynamicaly updates and returns the output based on the users operation choice
   let output;
   switch (operation) {
     case '1':
@@ -43,35 +49,44 @@ function performOperation(number1, number2, operation) {
   return output;
 }
 
-function invalidResponse(playAgain) {
+// TODO: FIX THIS FXN 
+function invalidPlayAgain(playAgain) {
+  // assigns playAgain to new variable called formatted, which formats it to all lowercase with white/blank space removed
   let formatted = playAgain.trim().toLowerCase();
+  // if the default language is french, the it checks the user's formatted response if its NOT oui or non
   if (defaultLanguage === 'fr') {
     return !['oui', 'non'].includes(formatted);
   } else {
+    // otherwise, check the user response to play again to see if it's 'yes' or 'no' 
     return !['yes', 'no'].includes(formatted);
   }
 }
 
 function invalidLanguageChoice(preferredLanguage) {
+  // checks the user's preferred language choice and returns a boolean if it is NOT the strings '1' or '2'
   return !['1', '2'].includes(preferredLanguage);
 }
 
 function setDefaultLanguage(preferredLanguage) {
+  // gets an index number based on the user's preferred language input to updated the default language
   let idx = Number(preferredLanguage) - 1;
+  // updates the default language
   defaultLanguage = LANGUAGES[idx];
 }
 
 function getInput() {
+  // gets input from the terminal and returns a String
   return READLINE.question();
 }
 
 function getMessage(defaultLanguage, messageTitle) {
+  // gets the defaultLanguage and message title keys and returns the value of the property on the messages object
   return MESSAGES[defaultLanguage][messageTitle];
 }
 
 // MAIN _____________________________________________
 
-prompt(getMessage(defaultLanguage, 'greeting'));
+prompt(getMessage(defaultLanguage, 'greeting')); // 
 
 prompt(getMessage(defaultLanguage, 'chooseLanguage'));
 
@@ -120,8 +135,8 @@ while (play) {
   prompt(getMessage(defaultLanguage,'playAgain'));
   let playAgain = getInput();
 
-  while (invalidResponse(playAgain)) {
-    prompt(getMessage(defaultLanguage, 'invalidResponse'));
+  while (invalidPlayAgain(playAgain)) {
+    prompt(getMessage(defaultLanguage, 'invalidPlayAgain'));
     playAgain = getInput();
   }
 
