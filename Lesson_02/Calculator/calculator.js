d15f0aa7ed6974a473677b0c4db3b809c4d6ae1d
Amanda <blayla89@gmail.com>
2024-05-03 14:06:28 -0500

    Refactored Calculator from hardcoded languages toward a more dynamic language change and message access approach

--- Lesson_02/Calculator/calculator.js
+++ Lesson_02/Calculator/calculator.js
@@ -2,35 +2,56 @@
 
 const READLINE = require('readline-sync');
 const MESSAGES = require('./calculator_messages.json');
-const VALID_OPERATIONS = ['1', '2', '3', '4']; 
+const VALID_OPERATIONS = ['1', '2', '3', '4'];
 let defaultLanguage = 'en';
-const LANGUAGES = ['en', 'fr'];
-let play = true;
+const VALID_LANGUAGES = MESSAGES[defaultLanguage]['languages'];
 
 // FXNS _______________________________________________
 
-function invalidNumber(number) {
-  // checks if number IS an empty or whitespace string OR IS  NaN => returns boolean (true if number IS invalid)
-  return number.trimStart() === '' || Number.isNaN(Number(number));
-}
-
-function invalidOperation(operation) {
-  // checks operation input -> returns true if it is NOT included in the string
-  return !VALID_OPERATIONS.includes(operation);
-}
-
 function prompt(message, variable) {
-  // checks if the variable is undefined, and if yes it outputs message as a string
   if (variable === undefined) {
     console.log(`=> ${message}`);
-  // if the variable is not empty, then it outputs the a more complex message AND the variable 
   } else {
     console.log(`=> ${message} ${variable}`);
   }
 }
 
+function getMessage(defaultLanguage, messageTitle) {
+  return MESSAGES[defaultLanguage][messageTitle];
+}
+
+function getInput() {
+  return READLINE.question();
+}
+
+function formatInput(input) {
+  return input.trim().toLowerCase();
+}
+
+function toProperCase(VALID_LANGUAGES) {
+  return VALID_LANGUAGES.map((language) => {
+    return language[0].toUpperCase() + language.slice(1);
+  });
+}
+
+function invalidLanguageChoice(languageChoice) {
+  return !VALID_LANGUAGES.includes(languageChoice);
+}
+
+function extractCharCode(languageChoice) {
+  return languageChoice.slice(0,2);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+function invalidOperation(operation) {
+  return !VALID_OPERATIONS.includes(operation);
+}
+
+
 function performOperation(number1, number2, operation) {
-  // takes 3 inputs to perform operation and dynamicaly updates and returns the output based on the users operation choice
   let output;
   switch (operation) {
     case '1':
@@ -49,64 +70,41 @@ function performOperation(number1, number2, operation) {
   return output;
 }
 
-// TODO: FIX THIS FXN 
 function invalidPlayAgain(playAgain) {
-  // assigns playAgain to new variable called formatted, which formats it to all lowercase with white/blank space removed
-  let formatted = playAgain.trim().toLowerCase();
-  // if the default language is french, the it checks the user's formatted response if its NOT oui or non
-  if (defaultLanguage === 'fr') {
-    return !['oui', 'non'].includes(formatted);
-  } else {
-    // otherwise, check the user response to play again to see if it's 'yes' or 'no' 
-    return !['yes', 'no'].includes(formatted);
-  }
+  const VALID_PLAY_AGAIN_INPUT = MESSAGES[defaultLanguage]['validPlayAgainInput'];
+  return !VALID_PLAY_AGAIN_INPUT.includes(playAgain);
 }
 
-function invalidLanguageChoice(preferredLanguage) {
-  // checks the user's preferred language choice and returns a boolean if it is NOT the strings '1' or '2'
-  return !['1', '2'].includes(preferredLanguage);
-}
-
-function setDefaultLanguage(preferredLanguage) {
-  // gets an index number based on the user's preferred language input to updated the default language
-  let idx = Number(preferredLanguage) - 1;
-  // updates the default language
-  defaultLanguage = LANGUAGES[idx];
-}
-
-function getInput() {
-  // gets input from the terminal and returns a String
-  return READLINE.question();
-}
-
-function getMessage(defaultLanguage, messageTitle) {
-  // gets the defaultLanguage and message title keys and returns the value of the property on the messages object
-  return MESSAGES[defaultLanguage][messageTitle];
-}
 
 // MAIN _____________________________________________
 
-prompt(getMessage(defaultLanguage, 'greeting')); // 
+prompt(MESSAGES[defaultLanguage]['greeting']);
 
-prompt(getMessage(defaultLanguage, 'chooseLanguage'));
+prompt(MESSAGES[defaultLanguage]['chooseLanguage'], toProperCase(VALID_LANGUAGES).join(', '));
 
-let preferredLanguage = READLINE.question();
+let languageChoice = formatInput(getInput());
 
-while (invalidLanguageChoice(preferredLanguage)) {
-  prompt(getMessage(defaultLanguage), 'invalidLanguage');
-  preferredLanguage = getInput();
+
+while (invalidLanguageChoice(languageChoice)) {
+  prompt(MESSAGES[defaultLanguage]['invalidLanguage'], toProperCase(VALID_LANGUAGES).join(', '));
+  languageChoice = formatInput(getInput());
+  console.clear();
 }
 
-setDefaultLanguage(preferredLanguage);
+if (languageChoice !== 'english') {
+  defaultLanguage = extractCharCode(languageChoice);
+}
 
-while (play) {
+while (true) {
 
   prompt(getMessage(defaultLanguage, 'firstNumber'));
-  let number1 = READLINE.question();
+
+  let number1 = getInput();
 
   while (invalidNumber(number1)) {
     prompt(getMessage(defaultLanguage, 'invalidNumber'));
     number1 = getInput();
+    console.clear();
   }
 
   prompt(getMessage(defaultLanguage, 'secondNumber'));
@@ -115,6 +113,7 @@ while (play) {
   while (invalidNumber(number2)) {
     prompt(getMessage(defaultLanguage,'invalidNumber'));
     number2 = getInput();
+    console.clear();
   }
 
   prompt(`${number1}, ${number2}`);
@@ -126,6 +125,7 @@ while (play) {
   while (invalidOperation(operation)) {
     prompt(getMessage(defaultLanguage, 'invalidOperation'));
     operation = getInput();
+    console.clear();
   }
 
   let operationResult = performOperation(number1, number2, operation);
@@ -133,17 +133,21 @@ while (play) {
   prompt(getMessage(defaultLanguage, 'result'), operationResult);
 
   prompt(getMessage(defaultLanguage,'playAgain'));
-  let playAgain = getInput();
+
+  let playAgain = formatInput(getInput());
+
 
   while (invalidPlayAgain(playAgain)) {
     prompt(getMessage(defaultLanguage, 'invalidPlayAgain'));
-    playAgain = getInput();
-  }
-
-  if (playAgain === 'no' || playAgain === 'non') {
-    play = false;
-    prompt(getMessage(defaultLanguage, 'thankyou'));
-  } else {
+    playAgain = formatInput(getInput());
     console.clear();
   }
+
+  if (playAgain !== MESSAGES[defaultLanguage]['validPlayAgainInput'][0]) {
+    prompt(getMessage(defaultLanguage, 'thankyou'));
+    break;
+  }
+
+  console.clear();
+
 }
